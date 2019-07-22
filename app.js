6faa6eaac36552b60cfdc7cb70640bfe19b320aa
Sai Natarajan <sainatarajan7@gmail.com>
2019-07-22 11:36:26 +0530

    first-version-release

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
 const bcrypt= require('bcryptjs')
@@ -9,15 +10,19 @@ const port = process.env.PORT || 5000
 const mongoUtil = require('./public/js/application/mongoutils.js')
 const app = express()
 const pathDir = path.join(__dirname, './public')
+const ObjectID= require('mongodb').ObjectID
 
+app.set('view engine', 'hbs')
 app.use(express.static(pathDir))
+app.use(session({secret: "dgs97tsag98sagh6sb124b", resave: false, saveUninitialized: true}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     next();
 });
 
@@ -73,8 +78,16 @@ function registerUser(email, password) {
     });
 }
 
-app.post('/dashboard', (req, res) => {
-    res.sendFile('./dashboard.html')
+app.get('/dashboard', (req, res) => {
+    if(!req.session.userID) {
+        console.log('Unauthorized user has entered')
+        return res.sendFile('unauthorized.html', { root: pathDir })
+    }
+    return res.render('dashboard', {'username': 'hi', 'userID': req.session.userID})
+})
+
+app.get('/login', (req, res) => {
+    return res.sendFile('login.html', { root: pathDir })
 })
 
 app.post('/validateUser', (req, res) => {
@@ -85,15 +98,36 @@ app.post('/validateUser', (req, res) => {
         email: email
     }).then((user) => {
         if(!user)
-            return res.send('invalid')
+            return res.send('no user found')
         else {
             bcrypt.compare(password, user.password, (err, result) => {
-                if(result === true)
-                    return res.send('valid')
+                if(result === true) {
+                    req.session.userID= user._id
+                    return res.redirect('/dashboard')
+                }
                 else
-                    return res.send('invalid')
+                    return res.send('hash comparison failed')
             })
         }
+    })
+})
+
+app.post('/userState', async (req, res) => {
+    var userID =req.body.userID
+    const db = mongoUtil.getDb();
+    var jsonObj= "{"
+    
+    const cursor= db.collection('devices').find({"user_id": ObjectID(userID)}).toArray((err, result) => res.send(result))
+    // while(await cursor.hasNext()) {
+    //     jsonObj+= cursor.next()
+    // }
+    // console.log("jsonObj : "+ jsonObj)
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((error) => {
+        console.log('Destroyed')
+        return res.redirect('/')
     })
 })
 
@@ -110,27 +144,6 @@ app.post('/register', (req, res) => {
         else {
             registerUser(email, password)
         }
-    })
-})
-
-app.post('/activate', (req, res) => {
-    var username = req.body.username
-    const db = mongoUtil.getDb();
-    const updatePromise = db.collection('users').updateOne({ name: username }, {
-        $inc: { switch: 1 }
-    }).then((result) => {
-        return res.send("activated")
-    }).catch((error) => {
-        return res.send("Error")
-    })
-})
-
-app.post('/getswitchvalue', (req, res) => {
-    var username = req.body.name
-    const db = mongoUtil.getDb();
-    db.collection('users').findOne({ name: username }, (error, user) => {
-        var value = user.switch % 2
-        return res.send(value.toString())
     })
 })
 
