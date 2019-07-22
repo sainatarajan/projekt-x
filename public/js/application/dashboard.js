494d5ea661a811e96064003cc8ef8d578a0eeb3d
Sai Natarajan <sainatarajan7@gmail.com>
2019-07-22 14:02:19 +0530

    first-version-release

--- public/js/application/dashboard.js
+++ public/js/application/dashboard.js
@@ -1,7 +1,7 @@
 window.onload= init
 
 function init() {
-    loadStateFromDatabase()
+    setInterval(loadStateFromDatabase, 2000)
 }
 
 function loadStateFromDatabase() {
@@ -34,5 +34,5 @@ function loadGUIState(data) {
 }
 
 function addEventListeners(data) {
-
+    
 }
