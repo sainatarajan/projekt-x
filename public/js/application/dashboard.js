181c9594b425fe1972085d012197b79fff5b0ff5
Sai Natarajan <sainatarajan7@gmail.com>
2019-07-22 11:55:22 +0530

    first-version-release

--- public/js/application/dashboard.js
+++ public/js/application/dashboard.js
@@ -72,7 +72,7 @@ function loadGUIState(data) {
         var deviceID= data[i].device_id.replace(/:/g, "")
         var deviceName= data[i].device_name
         var pinValues= data[i].device_pin_values
-        var rowDiv= $(".device-row").append("<div class=\"col-md-6\"><div class=\"tile appliance-tile\"><div class=\"tile-title-w-btn\"><b>"+deviceName+"</b><b>"+deviceID+"</b><br><br><p><button class=\"btn btn-primary icon-btn btn-appliance-add\"><i class=\"fa fa-plus\"></i>Appliance</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class=\"btn btn-secondary icon-btn btn-appliance-remove\"><i class=\"fa fa-minus\"></i>Remove Device</button></p></div><div class=\"row "+deviceID+"\"></div></div></div>")
+        var rowDiv= $(".device-row").append("<div class=\"col-md-12\"><div class=\"tile appliance-tile\"><div class=\"tile-title-w-btn\"><b>Device Name: "+deviceName+"</b><b>Device ID: "+deviceID+"</b><br><br><p><button class=\"btn btn-primary icon-btn btn-appliance-add\"><i class=\"fa fa-plus\"></i>Appliance</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class=\"btn btn-secondary icon-btn btn-appliance-remove\"><i class=\"fa fa-minus\"></i>Remove Device</button></p></div><div class=\"row "+deviceID+"\"></div></div></div>")
         for(var j= 0; j<pinValues.length; j++) {
             if(pinValues[j] == "0")
                 $("."+deviceID).append("<div class=\"col-md-3\"><b>Relay "+(j+1)+"</b><br><br><div class=\"toggle-flip\"><label><input type=\"checkbox\" class= \""+(deviceID + j)+"\"><span class=\"flip-indecator\" data-toggle-on=\"ON\"data-toggle-off=\"OFF\"></span></label></div></div></div>")
@@ -86,5 +86,5 @@ function loadGUIState(data) {
 }
 
 function addEventListeners(data) {
-    
+
 }
--- public/js/application/mongoutils.js
+++ public/js/application/mongoutils.js
@@ -1,8 +1,8 @@
 const MongoClient = require( 'mongodb' ).MongoClient;
 const uname1= 'sainatarajan'
 const pass1= 'we5Fw3AKA1gsHZWu'
-const databaseURL= 'mongodb+srv://'+uname1+':'+pass1+'@cluster0-pxh0h.mongodb.net/test?retryWrites=true&w=majority'
-//const databaseURL= 'mongodb://127.0.0.1:27017'
+//const databaseURL= 'mongodb+srv://'+uname1+':'+pass1+'@cluster0-pxh0h.mongodb.net/test?retryWrites=true&w=majority'
+const databaseURL= 'mongodb://127.0.0.1:27017'
 const databseName= 'projektx'
 
 var _db;
